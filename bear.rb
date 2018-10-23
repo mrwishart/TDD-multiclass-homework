1a1dc5d0c0638520c421dee0ba41048440df018a
Paul Wishart <misterwishart@gmail.com>
2018-10-23 17:00:49 +0100

    Added and successfully tested bear food count method

---
+++ bear.rb
@@ -0,0 +1,14 @@
+class Bear
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @stomach = []
+  end
+
+  def food_count()
+    return @stomach.count 
+  end
+
+end
---
+++ spec/bear_spec.rb
@@ -0,0 +1,51 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../river')
+require_relative('../fish')
+require_relative('../bear')
+
+class BearTest < MiniTest::Test
+
+  def setup
+
+    fish1 = Fish.new("Alan")
+    fish2 = Fish.new("Frank")
+    fish3 = Fish.new("Abby")
+    fish4 = Fish.new("Robert")
+    fish5 = Fish.new("Tom")
+
+    fishes = [fish1, fish2, fish3, fish4, fish5]
+
+    @river1 = River.new("Muddy", fishes)
+
+    @bear1 = Bear.new("Yogi")
+
+  end
+
+  def test_bear_name
+
+    expected = "Yogi"
+    actual = @bear1.name
+
+    assert_equal(expected, actual)
+
+  end
+
+  def test_bear_food_count
+
+    expected = 0
+    actual = @bear1.food_count()
+
+    assert_equal(expected, actual)
+
+  end
+
+  # def test_bear_take_fish__food_count
+  #
+  #   expected = 1
+  #   @bear1.eat(@river1)
+  #   actual = @bear1.food_count
+  #
+  # end
+
+end
